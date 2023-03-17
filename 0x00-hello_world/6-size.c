1ef783a6665c9dc046817dffe8c4ea219f1c6f59
Luckingz <luckyajidoku.com@gmail.com>
2023-03-17 05:07:03 -0700

    C programme that prints the size of various types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A programme to prints the size of various types
+ * on the computer it is compiled on
+ * Return: Ends the programme
+ */
+int main(void)
+{
+	printf("Size of a char: 1 bytes(s)\n
+			Size of an int: 4 byte(s)\n
+			Size of a long int: 4 byte(s)\n
+			Size of a long long int: 8 byte(s)\n
+			Size of a float: 4 byte(s)");
+	return (0);
+}
