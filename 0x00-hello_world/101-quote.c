09ff6273e93c41e6ef420bc2bf5e90ca17872444
Luckingz <luckyajidoku.com@gmail.com>
2023-03-17 08:00:23 -0700

    C programme to print a text

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,14 +7,6 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art useful\"
-		- Dora Korpar, 2015-10-19\n";
-	const char *ptr = message;
-	int length = 0;
-	while (*ptr)
-	{
-		length++;
-	}
-	write(STDERR_FILENO, message, length);
-	return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59;
+			return (1);
 }
