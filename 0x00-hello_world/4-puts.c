ade1c7fead492902e29e89fb8e8d14bd56a49a7b
Luckingz <luckyajidoku.com@gmail.com>
2023-03-16 23:16:56 -0700

    C programme to print using Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * Programme to print using puts command
  */
 int main(void)
@@ -8,7 +8,7 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 /** 
  * returns null
- * */
+ */
 return (0);
 }
 
