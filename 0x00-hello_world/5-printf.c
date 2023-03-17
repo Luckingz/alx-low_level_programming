85906245ebd3111cbdacde995c7f8c1b52fa5bc0
Luckingz <luckyajidoku.com@gmail.com>
2023-03-16 23:44:00 -0700

    A command to print using printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * A simple code to print a test using printf command
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n")
+return (0);
+}
