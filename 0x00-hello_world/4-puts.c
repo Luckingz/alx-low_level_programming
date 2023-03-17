5c70572e648503d24f152003de3fec851411f230
Luckingz <luckyajidoku.com@gmail.com>
2023-03-16 23:07:51 -0700

    C programme to print usign the Puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/**
+/** 
  * Programme to print using puts command
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+/** returns null*/
 return (0);
 }
 
