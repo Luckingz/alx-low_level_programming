94d7c75bb210ed22689e788362c0388d1f1bfc1d
Luckingz <luckyajidoku.com@gmail.com>
2023-03-18 12:38:39 -0700

    Programme to print all single digit numbers using purchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A programme that prints all single digit numbers of base 10
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int ch;
+
+	ch = 0;
+
+	while (ch < 10)
+	{
+		putchar(ch + '0');
+	}
+	putchar('\n');
+	return (0);
+}
