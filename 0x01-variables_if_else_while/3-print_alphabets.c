b56987ff6453a8199bed9fc6ec087ad8b402a56b
Luckingz <luckyajidoku.com@gmail.com>
2023-03-18 10:26:20 -0700

    Programme to print a-z and A-Z using while loop)

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - A programme to print lowercase alphabets and upper case alphabets
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	char ch1 = 'A';
+
+	while (ch1 <= 'Z')
+	{
+		putchar(ch1);
+		ch1++;
+	}
+	putchar('\n');
+	return (0);
+}
