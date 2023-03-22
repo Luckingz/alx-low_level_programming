6308a238e046e3da91d1c07d8015e1955aec072c
Luckingz <luckyajidoku.com@gmail.com>
2023-03-22 16:39:08 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,19 +4,21 @@
  * main - A programme to print alphabet
  * Return: 0 on success
  */
-void print_alphabet()
-{
-	char ch = 'a';
 
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
-	_putchar('\n');
-}
 int main(void)
 {
 	print_alphabet();
 	return (0);
+}
+
+void print_alphabet()
+{
+        char ch = 'a';
+
+        while (ch <= 'z')
+        {
+                _putchar(ch);
+                ch++;
+        }
+        _putchar('\n');
 }
