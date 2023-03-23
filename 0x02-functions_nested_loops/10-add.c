0d52c34ff001231f5751056420ef74dff43489ae
Luckingz <luckyajidoku.com@gmail.com>
2023-03-22 21:05:30 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - A programme that adds two integers
+ * Return: 0 on succss
+ * @int: numbers to be added
+ */
+
+int add(int, int)
+{
+	int a;
+	int b;
+	int result;
+
+	result = a + b;
+	printf("%d\n", result)
+		return (0);
+}
