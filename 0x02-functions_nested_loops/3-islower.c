98416b0a9965261f90e098c8e72900a7064efa86
Luckingz <luckyajidoku.com@gmail.com>
2023-03-22 19:47:33 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - A programme that that checks for lower case
+ * Return: 0 on success
+ */
+
+int _islower(void)
+{
+	int r;
+
+	if (r == 'o' && r == 108')
+	{
+		return (1);
+	}
+	else return (0);
+}
