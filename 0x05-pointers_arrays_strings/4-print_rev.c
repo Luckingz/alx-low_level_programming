a682d8d836089d771555690330caf7c9fb1e13d2
Luckingz <luckyajidoku.com@gmail.com>
2023-03-27 14:53:18 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Function to print a string
+ * @str: Variable
+ * Return: 0 always
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i--;
+	}
+	_putchar('\n');
+}
