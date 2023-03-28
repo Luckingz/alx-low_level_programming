b07c1d112ae4be3235ff75f15c5a95c062c811b8
Luckingz <luckyajidoku.com@gmail.com>
2023-03-28 12:55:11 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * puts2 - Function to print every other character of a string
+ * @str: variable
+ * Return: 0 always
+ */
+
+void puts2(char *str)
+{
+	len = _strlen(str);
+
+	int i = 0;
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+}
+
+/**
+ * _strlen - Function to print lenght of a String
+ * @s: s is a variable
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
