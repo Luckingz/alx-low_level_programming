5cff3d963a36b6b01c0e19081ebd42ee7286e865
Luckingz <luckyajidoku.com@gmail.com>
2023-03-28 13:36:44 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * puts_half - Function to print half a string
+ * @str: Variable
+ * Return: 0 always
+ */
+
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int i = len / 2;
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _strlen - Function to print lenght of a String
+ * @s: s is a variable
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
