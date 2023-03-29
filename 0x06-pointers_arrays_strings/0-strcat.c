1782d932b69cd1be26cb1c5a810fa5cd6825a2f4
Luckingz <luckyajidoku.com@gmail.com>
2023-03-29 14:57:57 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat: Function to concatenate two strings
+ * @dest: variable 1
+ * @src: variable 2
+ * Return: 0 always
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (result);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x06.c - More tasks on pointers, arrays, and strings
