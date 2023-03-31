4564a75667c24750b6a296089909dd0daebd2a4e
Luckingz <luckyajidoku.com@gmail.com>
2023-03-31 04:23:49 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - Function to copy a string
+ * @dest: Destination variable
+ * @src: Source variable
+ * @n: Counter variable
+ * Return: 0 always
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *result = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+	return (result);
+}
