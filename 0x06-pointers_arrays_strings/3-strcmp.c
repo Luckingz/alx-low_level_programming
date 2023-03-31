b9d8cf961c9b477982befc07d06a4ab8532b1a83
Luckingz <luckyajidoku.com@gmail.com>
2023-03-31 07:28:32 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function to compare two strings
+ * @s1: First string variable
+ * @s2: Second strign variable
+ * Return: 0 always
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (int)(*s1 - *s2);
+}
