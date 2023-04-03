f151b3083345d93a24bf28cf30c3840980dc8009
Luckingz <luckyajidoku.com@gmail.com>
2023-04-03 11:40:35 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - Function that copies memory area
+ * @dest: Destination variable
+ * @src: Source variable
+ * @n: Counter variable
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		src[i] = dest[i];
+		i++;
+	}
+	return (dest);
+}
