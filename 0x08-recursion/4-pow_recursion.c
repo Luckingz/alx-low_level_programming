56dc93a5d9fbaae7326529dbd8816ebad6c21ee8
Luckingz <luckyajidoku.com@gmail.com>
2023-04-06 10:16:33 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - A function to raise x to the power of y
+ * @x: Variable 1
+ * @y: Variable 2
+ * Return: 0 always
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+}
