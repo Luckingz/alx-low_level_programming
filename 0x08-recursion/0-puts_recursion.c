44dde5be7a9660e6783da3b771b28185c8e9b128
Luckingz <luckyajidoku.com@gmail.com>
2023-04-05 22:38:20 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - A function that prints a string
+ * @s: Varaiable of the string to be printed
+ * Return: 0 always
+ */
+
+void _puts_recursion(char *s)
+{
+	return (s);
+}
