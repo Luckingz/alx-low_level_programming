7891b32e290f217176aa8986811d55af44612845
Luckingz <luckyajidoku.com@gmail.com>
2023-04-06 15:34:56 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,11 +14,39 @@ int _sqrt_recursion(int n)
 	}
 	else if (n == 1)
 	{
-	return (1);
+		return (1);
 	}
 	else
 	{
-	return (-1);
+		return (-1);
+	}
+	return (0);
+}
+
+/**
+ * _pow_recursion - Function to calculate the power of a number
+ * @x: Variable number
+ * @y: Power number
+ * Return: 0 always
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
 	}
 	return (0);
 }
