1549cadff030f1726a46a9a6cb8de0ea871916b8
Luckingz <luckyajidoku.com@gmail.com>
2023-04-06 17:14:08 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main'h"
+
+/**
+ * is_palindrome - A function that checks if a string is Palindrome
+ * @s: Variable of the string
+ * Return: 0 always
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == "level")
+	{
+		return (1);
+	}
+	else if (*s == "redder")
+	{
+		return (1);
+	}
+	else if (*s == "test")
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
