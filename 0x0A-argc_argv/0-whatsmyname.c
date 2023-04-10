3e96f1d0c1c00401a4e919882934f414ed0a9e53
Luckingz <luckyajidoku.com@gmail.com>
2023-04-10 07:46:43 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Project to print the file name
+ * @argc: Argument counter
+ * @argv: Argument variable
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
