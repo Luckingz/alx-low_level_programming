63928d0cffa47ef24a7ea531476edfd652340294
Luckingz <luckyajidoku.com@gmail.com>
2023-04-10 07:57:09 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Programme to print number of arguments
+ * @argc: Argument counts
+ * @argv: Argument vector
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
