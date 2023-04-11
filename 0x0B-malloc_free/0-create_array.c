2ea96b79038affb03651fa45f1f2a9cba2d28770
Luckingz <luckyajidoku.com@gmail.com>
2023-04-11 16:59:57 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,8 @@ char *create_array(unsigned int size, char c)
 {
 	char *ch = malloc(size * sizeof(char));
 
-	if (ch == NULL)
-		return (NULL);
+	if (ch == '\0')
+		return ('\0');
 
 	unsigned int index;
 
