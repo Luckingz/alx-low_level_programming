eea629307eda8679191f858cd4b98a8520bd3df0
Luckingz <luckyajidoku.com@gmail.com>
2023-04-12 11:56:21 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,17 +11,39 @@
 
 char *_strdup(char *str)
 {
-	char *ch = malloc(sizeof(*str));
+	len = _strlen(str);
+
+	char *ch = malloc((len + 1) * sizeof(char));
 
 	if (str != '\0')
 	{
 		ch = str;
 		return (ch);
 	}
-	else if (str == '\0')
+	else if (ch == NULL)
 	{
-		return ('\0');
+		return (NULL);
 	}
 	free(ch);
 	return (0);
+}
+
+
+/**
+ * _strlen - A Fucntion that retuns the length
+ * of a string
+ * @s: Variable to be returned
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
