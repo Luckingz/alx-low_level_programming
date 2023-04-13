7adb1bac89b474c857b2f263e5846225b28b773a
Luckingz <luckyajidoku.com@gmail.com>
2023-04-13 11:56:24 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -23,22 +23,3 @@ void *malloc_checked(unsigned int b)
 	free(mem);
 	return (mem);
 }
-
-/**
- * _strlen - A Fucntion that retuns the length
- * of a string
- * @s: Variable to be returned
- * Return: always 0
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
