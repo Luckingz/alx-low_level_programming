64a999703c2df867c63826dd402e219d18dbc3d2
Luckingz <luckyajidoku.com@gmail.com>
2023-04-13 13:02:03 -0700

    1-string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,101 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Function that concatenates two strings
+ * @s1: First variable
+ * @s2: Second variable
+ * @n: Counter
+ * Return: 0 on succes
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = _strlen(s1);
+	unsigned int len2 = _strlen(s2);
+	unsigned int lentot = len1 + len2;
+
+	char *ch = malloc((lentot + 1) * sizeof(char));
+
+	char *result;
+
+	if (ch == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		result = _strncat(s1, s2, n);
+		_strcpy(ch, result);
+		return (ch);
+	}
+	free(ch);
+	return (ch);
+}
+
+/**
+ * _strncat - Function to concatenate two strings
+ * @dest: Destination variable
+ * @src: Source variable
+ * @n: variable 3
+ * Return: 0 always
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *result = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+	return (result);
+}
+
+/**
+ * _strcpy - Function that copies a string to another
+ * @dest: Destination variable
+ * @src: Source variable
+ * Return: 0 always
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (*src != '\0')
+	{
+		*dest++ = *src++;
+	}
+	*dest = '\0';
+	return (result);
+}
+
+/**
+ * _strlen - A Fucntion that retuns the length
+ * of a string
+ * @s: Variable to be returned
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
