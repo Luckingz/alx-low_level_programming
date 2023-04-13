ad99fd3bbb04cd970423ccdc55d46fab2e65e55a
Luckingz <luckyajidoku.com@gmail.com>
2023-04-13 11:28:17 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Function that allocates memory using malloc
+ * @b: Interger variable
+ * Return: 0 on success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int len = sizeof(b);
+	int *mem = malloc(len * sizeof(int));
+
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+	else if (mem != 0)
+	{
+		return (mem);
+	}
+	else
+	{
+		exit(98);
+	}
+	free(mem);
+	return (0);
+}
+
+/**
+ * _strlen - A Fucntion that retuns the length
+ * of a string
+ * @s: Variable to be returned
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
