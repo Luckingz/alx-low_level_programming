38b4c40f82cef3386c33d10d56d99c0699759c7a
Luckingz <luckyajidoku.com@gmail.com>
2023-04-15 12:45:37 -0700

    libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,18 +2,16 @@
 
 /**
  * _puts - Function to print a string
- * @str: Variable
+ * @s: Variable
  * Return: 0 always
  */
 
 void _puts(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(*s);
+		s++;
 	}
 	_putchar('\n');
 }
