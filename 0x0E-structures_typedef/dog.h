f0099d65e699e6f39c09d659445fd4f9e6c7fa9e
Luckingz <luckyajidoku.com@gmail.com>
2023-04-17 16:53:47 -0700

    Structure for Dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct Dog - A structure for Dogs
+ * @name: Name of Dogs
+ * @age: Age of Dogs
+ * @owner: Owner of Dogs
+ *
+ * Description: A structure for Dogs of different owners, breed, age and names;
+ */
+
+struct Dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
