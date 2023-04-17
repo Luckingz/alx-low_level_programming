29412968ca57d1be43c97ce3a0d368f50dbb02c4
Luckingz <luckyajidoku.com@gmail.com>
2023-04-17 14:27:40 -0700

    Programe that prints the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#ifndef MAIN_C
+#define MAIN_C
+
+/**
+ * main - Programme that prints the name of the file
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+#endif
+	return (0);
+}
