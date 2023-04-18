e89197745e7574d3a51ce10b03733e58776d1028
Luckingz <luckyajidoku.com@gmail.com>
2023-04-17 17:57:31 -0700

    Dog strcuture

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		*name = &name;
+		age = age;
+		*owner = &owner;
 	}
 	return (0);
 }
