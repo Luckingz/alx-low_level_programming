8c12fb13ea0970dfe14f80cb1b9c6c3cf5aeab84
Luckingz <luckyajidoku.com@gmail.com>
2023-04-17 17:29:56 -0700

    Structure of Dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - Function to initialize variable of type struct dog
+ * @d: Variable 1
+ * @name: Name of Dog
+ * @age: Age of Dog
+ * @owner: Owner of Dog
+ * Return: 0 on success
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog;
+
+	return (0);
+}
