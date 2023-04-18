efea4c7f4a6fe207c26c754d3ffad7fbdcb6f500
Luckingz <luckyajidoku.com@gmail.com>
2023-04-17 17:13:32 -0700

    A structure for Dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #ifndef DOG_H
 #define DOG_H
 
 /**
- * struct Dog - A structure for Dogs
+ * struct dog - A structure for Dogs
  * @name: Name of Dogs
  * @age: Age of Dogs
  * @owner: Owner of Dogs
@@ -11,13 +10,10 @@
  * Description: A structure for Dogs of different owners, breed, age and names;
  */
 
-struct Dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-typedef struct Dog dog;
-
 #endif
