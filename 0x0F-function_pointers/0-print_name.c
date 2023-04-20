8d0d5e72a8b4034ca70316f2553c8f6aef026f8e
Luckingz <luckyajidoku.com@gmail.com>
2023-04-20 02:53:34 -0700

    A function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - Function that prints a name
+ * @name: Name to be printed
+ * @f: variable
+ * Return: 0 on success
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	name = &void (*f)(char *);
+	int index = 0;
+
+	while (*name != '\0')
+	{
+		_putchar(name[index])
+			index++;
+	}
+	return (0);
+}
