9755c192463575400a7d6f65ae0b9ed08e796d2c
Luckingz <luckyajidoku.com@gmail.com>
2023-04-20 16:33:43 -0700

    Function to print a string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,8 +29,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 		{
 			printf("%s", ch);
-			printf("%s", separator);
-			printf(" ");
+			if (i != n - 1 && separator != NULL)
+			{
+				printf("%s", separator);
+				printf(" ");
+			}
 		}
 	}
 		printf("\n");
